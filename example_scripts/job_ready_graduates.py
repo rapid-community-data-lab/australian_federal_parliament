0ddee89d69c088ff66887ed9b69bd966600d4e37
Sam Hames <s.hames@uq.edu.au>
2026-02-24 13:11:19 +1000

    Add visual speech demarcation, and handle interjections by the non-nominal speaker

--- example_scripts/job_ready_graduates.py
+++ example_scripts/job_ready_graduates.py
@@ -25,13 +25,28 @@ import datetime
 import sqlite3
 
 from openpyxl import Workbook
-from openpyxl.worksheet.hyperlink import Hyperlink
+from openpyxl import styles
 
 # Note - isolation_level=None won't work in future versions of Python (sometime after
 # 3.13), this should be using the autocommit=True value instead, but requires some
 # investigation for different python versions
 db = sqlite3.connect("oz_federal_hansard.db", isolation_level=None)
 
+# Create a temporary table to handle interjections as CSS classes - this is a temporary
+# measure until we've resolved all of the speaker identification issues.
+db.executescript("""
+    CREATE temporary table contains_interjection (
+        para_id integer primary key,
+        interjection bool
+    );
+
+    insert into contains_interjection
+        select distinct
+            para_id, 
+            1
+        from paragraph_enclosed_class
+        where lower(class) like '%inter%';
+    """)
 
 query = """
     WITH matching_debate as (
@@ -54,15 +69,14 @@ query = """
         where lower(paragraph_text) glob '*job?ready graduates*' 
             and date >= '2020-01-01'
     )
-
     select 
         session.date,
         session.chamber,
         session.transcript_pdf_url as full_transcript_link,
         debate.title as debate_title,
         fragment_number as speech_number,
-        parliamentarian.display_name,
-        party.name as party,
+        case when interjection then 'interjector' else parliamentarian.display_name end,
+        case when interjection then null else party.name end,
         paragraph.paragraph_text,
         lower(paragraph_text) glob '*job?ready graduates*' as matches_phrase
     from paragraph
@@ -73,12 +87,15 @@ query = """
         and session.date between party_member.start_date and 
             coalesce(party_member.end_date, '3000-01-01')
     left outer join party using(party_id)
+    left outer join contains_interjection using(para_id)
+    -- Find interjections using the classes assigned to paragraphs, as a workaround
+    -- in advance of figuring out all the nuances of speakers...
     where (paragraph.session_id, fragment_number) in (
         select session_id, fragment_number from matching_speech
     ) or debate_id in (
         select debate_id from matching_debate
     )
-    order by session.date
+    order by session.date, chamber, speech_number
 
 """
 
@@ -108,6 +125,27 @@ for row in all_rows:
 
     row[2].hyperlink = link
     row[2].value = "Session Transcript"
+
+# Zebra stripe speeches.
+all_rows = worksheet.rows
+next(all_rows)  # skip header
+
+colour = True
+last_speech = (None, None, None)
+
+solid_fill = styles.PatternFill(fill_type="solid", fgColor="efefef")
+
+for row in all_rows:
+
+    current_speech = (row[0].value, row[1].value, row[4].value)
+
+    if current_speech != last_speech:
+        colour = not colour
+        last_speech = current_speech
+
+    if colour:
+        for cell in row:
+            cell.fill = solid_fill
 
 
 workbook.save("job_ready_graduates_speeches.xlsx")
